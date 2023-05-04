7e45845712a050d035ea083accc6bef899bb0aeb
Grogu-e <85890933+Grogu-e@users.noreply.github.com>
2023-05-04 13:36:37 -0400

    Update schema.sql

--- tastytable/schema.sql
+++ tastytable/schema.sql
@@ -17,21 +17,32 @@ CREATE TABLE post(
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
 
+CREATE TABLE recipe (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+)
+
+
 CREATE TABLE recipe_ingredents
 (recipe_id_fk INTEGER NOT NULL,
 ingredent_id_fk INTEGER NOT NULL,
 quantity INTEGER NOT NULL,
 measure_type TEXT NOT NULL,
 FOREIGN KEY (recipe_id_fk) REFERENCES recipe (id)
-FOREIGN KEY (recipe_id_fk) REFERENCES recipe (id)
+FOREIGN KEY (ingredent_id_fk) REFERENCES ingredients (id_pk)
 );
 
 
 CREATE TABLE ingredients 
 (
-id_ pk INTEGER NOT NULL,
-type   TEXT NOT NULL
+    id_pk INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL
 );
+
 
 CREATE TABLE steps
 (
